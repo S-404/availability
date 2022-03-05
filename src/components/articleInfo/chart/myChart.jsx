15d5420997760f09d185ba4f21d0f21d88397404
Andrey <91373803+S-404@users.noreply.github.com>
2022-03-06 02:55:05 +0300

    add ChartJS

---
+++ src/components/articleInfo/chart/myChart.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import {Line} from 'react-chartjs-2';
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+const MyChart = ({graph}) => {
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Line Chart',
+            },
+        },
+    };
+
+    const labels = graph?.FC.map(wk=>wk.wk)
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Forecast',
+                data: graph?.FC.map(wk=>wk.value),
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'Sold',
+                data: graph?.SOLD.map(wk=>wk.value),
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    };
+
+
+    return (
+        <div className='article-info__chart'>
+            <Line options={options} data={data}/>
+        </div>
+
+)
+    ;
+};
+
+export default MyChart;
