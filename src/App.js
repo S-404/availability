ea2314ac69f677bf1a4450046d2607a006d7fb2e
Andrey <91373803+S-404@users.noreply.github.com>
2022-03-05 20:22:57 +0300

    upd App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import './styles/App.css';
+import ArticleListFilters from "./components/articleListFilters/ArticleListFilters";
+import ArticleList from "./components/articleList/ArticleList";
+import {useFetching} from "./hooks/useFetching";
+import ArticleListService from "./services/articleListService";
+import {useDispatch} from "react-redux";
 
 function App() {
-  return (
-    <div className="App">
-   works
-    </div>
-  );
+    const dispatch = useDispatch();
+    const [fetchArticleList, isFetchingArticleList, fetchingArticleListError] = useFetching(async (param) => {
+        const responseData = await ArticleListService.getArticleList(param)
+        dispatch({type: 'SET_LIST', value: responseData})
+    })
+
+
+    return (
+        <div className="App">
+            <ArticleListFilters
+                fetchArticleList={fetchArticleList}
+            />
+            <ArticleList
+                isFetchingArticleList={isFetchingArticleList}
+                fetchingArticleListError={fetchingArticleListError}
+            />
+        </div>
+    );
 }
 
 export default App;
