aaba73a521533154fba1695107158e6221a59280
Andrey <91373803+S-404@users.noreply.github.com>
2022-03-06 20:15:57 +0300

    fix useArticleListPagination

--- src/hooks/useArticleList.js
+++ src/hooks/useArticleList.js
@@ -17,12 +17,9 @@ export const useArticleListPagination = (articleList, paginator) => {
     // paginator:{ currentPage, maxPage, pageLimit}
     return useMemo(() => {
         if (articleList.length) {
-            const to = paginator.currentPage * paginator.pageLimit;
-            const from = to - paginator.pageLimit;
-
+            const to = Math.max(paginator.currentPage * paginator.pageLimit, paginator.pageLimit);
+            const from = Math.max( to - paginator.pageLimit,0);
             return articleList.slice(from, to);
-
-
         }
         return articleList
     }, [articleList, paginator.currentPage])
